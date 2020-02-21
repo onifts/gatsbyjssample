a88fc67a1efb579fbb9110ed07a1486193ef31eb
Caprica <frigatebirdmanager@gmail.com>
2020-02-21 22:09:07 +0900

    material ui 샘플 코드 만들기

---
+++ gatsby-sample/src/pages/samplemtui/mt12.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default function ContextMenu() {
+  
+  return (
+    <div onContextMenu={(e)=> e.preventDefault()} >
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ipsum purus, bibendum sit
+        amet vulputate eget, porta semper ligula. Donec bibendum vulputate erat, ac fringilla mi
+        finibus nec. Donec ac dolor sed dolor porttitor blandit vel vel purus. Fusce vel malesuada
+        ligula. Nam quis vehicula ante, eu finibus est. Proin ullamcorper fermentum orci, quis
+        finibus massa. Nunc lobortis, massa ut rutrum ultrices, metus metus finibus ex, sit amet
+        facilisis neque enim sed neque. Quisque accumsan metus vel maximus consequat. Suspendisse
+        lacinia tellus a libero volutpat maximus.
+      </Typography>
+    </div>
+  );
+}
