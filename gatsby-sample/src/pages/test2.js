6461ca217ea9dcdee8ce1a698c399d4130000933
caprica <frigatebirdmanager@gmail.com>
2020-03-11 10:14:06 +0900

    코맨트 보완

--- gatsby-sample/src/pages/test2.js
+++ gatsby-sample/src/pages/test2.js
@@ -15,34 +15,61 @@ const useStyles = makeStyles(theme => ({
 
 }));
 
+const validateForm = (errors) => {
+  
+    let valid = true;
+    
+    Object.values(errors).forEach( (val) => val.length > 0 && (valid = false) );
+    
+    return valid;
+  
+} // end validateForm
+
+const initStateUserInfo = {
+    
+    username : ''
+
+};
+
 const Test = () => {
     
     const classes = useStyles();
 
+    const [ stateUserInfo, setStateUserInfo ] = useState(initStateUserInfo);
     const [ openDialogForm, setOpenDialogForm ] = useState(false);
-    const [ stateUserInfo, setStateUserInfo ] = useState({username:''});
-
+    
     const handleDialogFormOpen = ( event ) => {
 
         event.preventDefault();
         
         setOpenDialogForm(true);
         
-    }; // end handlePopMenuOpen
+    }; // end handleDialogFormOpen
   
     const handleDialogFormClose = ( event, param ) => {
   
         event.preventDefault();
         
-        if ( event.currentTarget.id === 'userinfo' ) {
-            
-            setStateUserInfo( { ...stateUserInfo, username: param.inputValue } );
+        if( param !== undefined && param.inputValue !== undefined ) {
+
+            if( !validateForm(param.errors) ) {
+          
+                alert('Please input a valid value.');
+                return;
+                
+            } // end if 
+    
+            if ( event.currentTarget.id === 'userinfo' ) {
+                
+                setStateUserInfo( { ...stateUserInfo, username: param.inputValue } );
+    
+            } // end if 
 
         } // end if 
 
         setOpenDialogForm(false);
 
-    }; // end handlePopMenuClose
+    }; // end handleDialogFormClose
 
     return (
 
