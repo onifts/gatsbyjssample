49d23f26c2aa249178d201a842c18d8250c0b145
caprica <frigatebirdmanager@gmail.com>
2020-03-25 00:57:12 +0900

    (gatsby)(20200325)s local image show sample

---
+++ gatsby-sample/src/pages/test51.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import icon from '../images/gatsby-icon.png'
+
+class test5 extends Component {
+
+    render() {
+        return (
+            <div style={{border:'1px solid', height:'200px'}}>
+                <div style={{border:'1px solid', height:'100px', width:'100px', margin:'10px'}}>
+                
+                    <img src={icon}/>
+
+                </div>
+            </div>
+        );
+    }
+
+} // end TestF
+
+export default test5;
